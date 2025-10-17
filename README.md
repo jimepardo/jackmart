fbc17fe8c766892c53b871088b6331dc1bd37f0e
Jime Pardo <jimenapardo@outlook.com>
2025-10-16 22:36:27 -0300

    Pre-delivery: Initial project setup and routing logic

---
+++ README.md
@@ -0,0 +1,34 @@
+# Pre-release Node.js
+
+## Obtain all products
+
+```shell
+npm run start GET products
+```
+
+## Obtain an specific product
+
+```shell
+npm run start GET products/15
+```
+
+## Add a new product
+
+```shell
+npm run start POST products "T-Shirt" 200 "T-Shirts"
+```
+
+## Modified product
+
+```shell
+npm run start PUT products "T-Shirt" 199 "T-Shirts"
+```
+
+## Delete product
+
+```shell
+npm run start DELETE products/15
+```
+
+### Author
+- Pardo, Jimena
---
+++ index.js
@@ -0,0 +1,74 @@
+import { deleteProduct, modifiedProduct, createProduct, getProductById, getProducts } from "./callbacks.js";
+
+let [, , method, resource, ...params] = process.argv;
+
+method = method.toUpperCase();
+resource = resource.toLowerCase();
+
+const [title, price, category] = params;
+
+const productData = {
+    title,
+    price,
+    category
+};
+
+async function processRequest(method, resource, productData) {
+
+    const isResourcePresent = resource.startsWith("products");
+    const id = parseInt(resource.split("/")[1]);
+
+    if (isResourcePresent && resource !== "products" && resource.split("/").length === 2) {
+        if (isNaN(id) || id <= 0) {
+            console.error("Invalid ID. The product does not exist.");
+            return;
+        }
+
+        switch (method) {
+            case "GET":
+                const productFound = await getProductById(id);
+                if (productFound) {
+                    console.log("Product found: ", productFound);
+                    return { status: 200, data: productFound };
+                }
+                else {
+                    console.error("Product not found.");
+                    return { status: 404, message: "Product not found." };
+                }
+            case "PUT":
+                console.log("Modifying product...");
+                await modifiedProduct(id, productData);
+                return { status: 200, message: "Product updated successfully." };
+            case "DELETE":
+                console.log("Deleting product...");
+                await deleteProduct(id);
+                return { status: 200, message: "Product deleted successfully." };
+
+            default:
+                console.error("Invalid method. Use GET, POST, PUT or DELETE.");
+                return;
+        }
+
+    }
+    if (resource === "products") {
+        switch (method) {
+            case "GET":
+                console.log("Fetching all products...");
+                const allProducts = await getProducts();
+                return { status: 200, message: "Products retrieved successfully.", allProducts };
+            case "POST":
+                console.log("Creating product...");
+                await createProduct(productData);
+                return { status: 201, message: "Product created successfully." };
+            default:
+                console.error("Invalid method. Use GET or POST.");
+                return;
+        }
+    }
+    else {
+        console.error("Invalid resource. Use 'products' or 'products/{id}'.");
+        return;
+    }
+}
+
+processRequest(method, resource, productData);
